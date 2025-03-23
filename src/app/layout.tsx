89ea2e5c1c192c15419aad20a3d23683403f530e
fatemekoushki <fatemekoushki3@gmail.com>
2025-03-23 20:00:02 +0430

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en"  dir="rtl">
+      <head>
+      <link rel="icon" type="image/png" href="./carto.jpeg"  />
+      </head>
       <body
         
       >
